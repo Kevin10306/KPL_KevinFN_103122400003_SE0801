2256e3ab28baaea53970810c6cabdedf7853dea4
Kevin Ferdinand <kevin.ferdin4nd@gmail.com>
2026-05-19 20:18:06 +0700

    TP 12

---
+++ 12_Performance_Analysis_Unit_Testing_dan_Debugging/TP/index.js
@@ -0,0 +1,22 @@
+function main() {
+    const data = ["123", 456, "hello", 78.9, true];
+
+    for (let i = 0; i < data.length; i++) {
+        const result = processData(data[i]);
+        console.log(`Item ${i + 1}: ${data[i]} -> ${result}`);
+    }
+}
+
+function processData(data) {
+    if (typeof data === "boolean") return `Boolean: ${data}`;
+
+    const str = String(data).toLowerCase();
+    const num = Number(str);
+
+    if (!isNaN(num)) {
+        return `Number: ${num * 2}`;
+    }
+    return `Teks: ${str} (panjangnya: ${str.length})`;
+}
+
+main();
